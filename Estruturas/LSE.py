02ebd1979eedd1125319fd746be9b31c4e4168f6
Emanuelli Prigol Ceresoli <marcelocerezoli4@gmail.com>
2026-09-06 16:24:26 -0300

    Adiciona métodos de impressão e verificação da LSE

--- Estruturas/LSE.py
+++ Estruturas/LSE.py
@@ -6,3 +6,16 @@ class LSE:
         self.inicio = None
         self.fim = None
         self.total = 0
+
+    def esta_vazia(self):
+        return self.inicio is None
+
+    def inserir_inicio(self, valor):
+        novo_nodo = Nodo(valor)
+
+        if self.esta_vazia():
+            self.inicio = novo_nodo
+            self.fim = novo_nodo
+        else:
+            novo_nodo.proximo = self.inicio
+            self.inicio = novo_nodo
