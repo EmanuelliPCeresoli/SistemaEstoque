fa584f2e3acc176df5b64423ecee331758174d90
Emanuelli Prigol Ceresoli <marcelocerezoli4@gmail.com>
2026-09-08 09:46:23 -0300

    Finalização da pasta LDE.

--- Estruturas/LDE.py
+++ Estruturas/LDE.py
@@ -72,3 +72,27 @@ class LDE:
 
     def imprimir_lista(self):
         atual = self.inicio
+
+        while atual is not None:
+            print(atual)
+            atual = atual.proximo
+
+    def imprimir_reverso(self):
+        atual = self.fim
+
+        while atual is not None:
+            print(atual)
+            atual = atual.anterior
+
+    def imprimir_horizontal(self):
+        atual = self.inicio
+        elementos = []
+
+        while atual is not None:
+            elementos.append(f"[{atual}]")
+            atual = atual.proximo
+
+        print(" <-> ".join(elementos))
+
+    def tamanho(self):
+        return self.total
