4b3d5d1f33800f94daa1db20d38e211cfb826d51
Emanuelli Prigol Ceresoli <marcelocerezoli4@gmail.com>
2026-09-08 09:01:35 -0300

    Implemntação de alguns elementos que devem ser mudados na pasta nodo.

--- Estruturas/LDE.py
+++ Estruturas/LDE.py
@@ -25,3 +25,27 @@ class LDE:
 
     def inserir_fim(self, valor):
         novo = Nodo(valor)
+
+        if self.esta_vazia():
+            self.inicio = novo
+            self.fim = novo
+        else:
+            novo.anterior = self.fim
+            self.fim.proximo = novo
+            self.fim = novo
+
+            self.total += 1
+
+    def remover_inicio(self):
+        if self.esta_vazia():
+            return "A lista está vazia."
+
+        removido = self.inicio
+
+        if self.inicio == self.fim:
+            self.inicio = None
+            self.fim = None
+        else:
+            self.inicio = self.inicio.proximo
+            self.inicio.anterior = None
+            removido.proximo = None
