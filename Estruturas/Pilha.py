bc9e3eb2aa69b6e61485f2a9a3d38c989b8aaa81
Emanuelli Prigol Ceresoli <marcelocerezoli4@gmail.com>
2026-09-08 08:21:29 -0300

    Conclusão da primeira parte da Pilha

--- Estruturas/Pilha.py
+++ Estruturas/Pilha.py
@@ -6,3 +6,11 @@ class Pilha:
     def push(self, valor):
         """Empilha um novo elemento no topo."""
         self.elementos.append(valor)
+
+    def pop(self):
+        """Remove e retorna o elemento no topo."""
+        if self.esta_vazia():
+            raise IndexError("A pilha está vazia")
+
+        topo = self.elementos.pop()
+        return topo
