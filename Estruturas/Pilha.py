52911fe5e538318feb528d1a4daccb942ba43d41
Emanuelli Prigol Ceresoli <marcelocerezoli4@gmail.com>
2026-09-08 08:33:49 -0300

    Finalização da pasta Pilha depois de adicionar métodos utilitários da Pilha.

--- Estruturas/Pilha.py
+++ Estruturas/Pilha.py
@@ -14,3 +14,26 @@ class Pilha:
 
         topo = self.elementos.pop()
         return topo
+
+    def peek(self):
+        """Retorna ao topo sem remover."""
+        if self.esta_vazia():
+            raise IndexError("A Pilha está vazia.")
+
+        return self.elementos[-1]
+
+    def esta_vazia(self):
+        return not self.elementos
+
+    def limpar(self):
+        """Remove todos os elementos da Pilha."""
+        self.elementos.clear()
+
+    def __len__(self):
+        return self.tamanho()
+
+    def __str__(self):
+        if self.esta_vazia():
+            return "Pilha: []"
+
+        return "Topo -> " + " | ".join(map(str, reversed(self.elementos)))
