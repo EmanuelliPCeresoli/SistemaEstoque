2937a268c79c52a9e54a993fdc4ad5952f2b115f
Emanuelli Prigol Ceresoli <marcelocerezoli4@gmail.com>
2026-09-09 20:05:58 -0300

    Correção de erros e implementação de objetos que faltavam.

--- Estruturas/LSE.py
+++ Estruturas/LSE.py
@@ -1,4 +1,4 @@
-import Nodo
+from Nodo import Nodo
 
 class LSE:
 
@@ -9,6 +9,16 @@ class LSE:
 
     def esta_vazia(self):
         return self.inicio is None
+
+    def buscar(self, codigo):
+        atual = self.inicio
+
+        while atual is not None:
+            if atual.dado.get_identificador_unico() == codigo:
+                return atual.dado
+            atual = atual.proximo
+
+        return None
 
     def inserir_inicio(self, valor):
         novo_nodo = Nodo(valor)
@@ -29,13 +39,53 @@ class LSE:
             self.inicio = novo_nodo
             self.fim = novo_nodo
         else:
-            novo_nodo.proximo = self.inicio
-            self.inicio = novo_nodo
+            self.fim.proximo = novo_nodo
+            self.fim = novo_nodo
 
         self.total +=1
 
+    def remover(self, codigo):
+        if self.esta_vazia():
+            return None
+
+        if self.inicio.dado.get_identificador_unico() == codigo:
+            return self.remover_inicio()
+
+        anterior = self.inicio
+        atual = self.inicio.proximo
+
+        while atual is not None:
+            if atual.dado.get_identificador_unico() == codigo:
+                anterior.proximo = atual.proximo
+
+                if atual == self.fim:
+                    self.fim = anterior
+
+                atual.proximo = None
+                self.total -1
+                return atual.dado
+
+            anterior = atual
+            atual = atual.proximo
+
+        return None
+
     def remover_inicio(self):
-        if self.esta_vazia(self):
+        if self.esta_vazia():
+            return "A lista está vazia"
+
+        removido = self.inicio
+        self.inicio = self.inicio.proximo
+
+        if self.inicio is None:
+            self.fim = None
+
+        removido.proximo = None
+        self.total -1
+        return removido
+
+    def remover_fim(self):
+        if self.esta_vazia():
             return "A lista esta vazia."
 
         removido = self.inicio
@@ -68,11 +118,11 @@ class LSE:
         atual = self.inicio
         elementos = []
 
-        while atual is not  None:
+        while atual is not None:
             elementos.append(f"[{atual}]")
             atual = atual.proximo
 
-            print(" -> ".join(elementos))
+        print(" -> ".join(elementos))
 
     def tamanho(self):
         return self.total
